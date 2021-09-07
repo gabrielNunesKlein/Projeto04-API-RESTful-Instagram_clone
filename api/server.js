209a72ed717f7b0aafcd587e18ce088bf085e12d
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-09-07 19:53:18 -0300

    Inserindo documentos no mongoDB

--- api/server.js
+++ api/server.js
@@ -7,7 +7,30 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = 80;
+var port = 8080;
 app.listen(port);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
 console.log("Servidor HTTP, está escutando na porta ", port);
+
+app.post('/api', function(req, res) {
+    var dados = req.body;
+
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json({'status': 'erro'});
+                } else {
+                    res.json({'status': 'Inclusão realizada com sucesso'});
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
