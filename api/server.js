c2c01fd7d979c96f81cf8a71fe01a1d84454c42e
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-09-07 20:51:19 -0300

    Removendo documentos por ID no mongoDB

--- api/server.js
+++ api/server.js
@@ -87,3 +87,19 @@ app.put('/api/:id', function(req, res){
         });
     });
 });
+
+app.delete('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.remove({ _id: objectId(req.params.id)}, function(err, records){
+
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(records);
+                    // res.status(500).json(records);
+                }
+            });
+        });
+    });
+});
